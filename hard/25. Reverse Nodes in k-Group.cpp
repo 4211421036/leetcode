b817d33eb119081b905b739809b39ebf7442db57
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-04-20 22:18:41 +0700

    Create 25. Reverse Nodes in k-Group.cpp

---
+++ hard/25. Reverse Nodes in k-Group.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || k == 1) return head;
+        
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+        ListNode* curr = head;
+        ListNode* next = nullptr;
+        
+        int count = 0;
+        ListNode* temp = head;
+        while (temp) {
+            temp = temp->next;
+            count++;
+        }
+        
+        while (count >= k) {
+            curr = prev->next;
+            next = curr->next;
+            for (int i = 1; i < k; i++) {
+                curr->next = next->next;
+                next->next = prev->next;
+                prev->next = next;
+                next = curr->next;
+            }
+            prev = curr;
+            count -= k;
+        }
+        
+        return dummy->next;
+    }
+};
