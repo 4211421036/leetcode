bc99953f4d5ad96e820c83503391f5412f7b8862
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-04-21 06:44:45 +0700

    Create 20. Valid Parentheses.cpp

---
+++ easy/20. Valid Parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> mapping = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+        
+        for (char c : s) {
+            if (mapping.find(c) != mapping.end()) {
+                // Closing bracket
+                if (st.empty() || st.top() != mapping[c]) {
+                    return false;
+                }
+                st.pop();
+            } else {
+                // Opening bracket
+                st.push(c);
+            }
+        }
+        
+        return st.empty();
+    }
+};
