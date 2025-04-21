a48391aa7f73b3c525c53a7c780fb6c48523252d
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-04-21 07:12:54 +0700

    Create 38. Count and Say.cpp

---
+++ medium/38. Count and Say.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string current = "1";
+        
+        for (int i = 2; i <= n; i++) {
+            string next;
+            int count = 1;
+            
+            for (int j = 1; j < current.size(); j++) {
+                if (current[j] == current[j-1]) {
+                    count++;
+                } else {
+                    next += to_string(count) + current[j-1];
+                    count = 1;
+                }
+            }
+            next += to_string(count) + current.back();
+            current = next;
+        }
+        
+        return current;
+    }
+};
